fe8e75022ef9af73d536d3bdd9142f74fbeaeef4
Ah95ed <amhmeed31@gmail.com>
2025-08-05 00:20:14 +0300

    edit some code in project

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:wishcrafted/Controller/AccessibilityProvider/AccessibilityProvid
 import 'package:wishcrafted/Helper/LogApp/LogApp.dart';
 import 'package:wishcrafted/Helper/Service/initService.dart';
 import 'package:wishcrafted/Helper/TranslationApp/LanguageTranslation.dart';
-import 'package:wishcrafted/View/SpecialNeedsScreen.dart';
 import 'package:wishcrafted/View/SplashScreen/SplashScreen.dart';
 import 'package:wishcrafted/View/style/AppColors/AppColors.dart';
 import 'package:wishcrafted/View/style/SizeApp/ScreenSize.dart';
@@ -43,7 +42,6 @@ class WishCraftedApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Consumer<AccessibilityProvider>(
       builder: (c, value, child) {
         return SizeBuilder(
@@ -74,7 +72,7 @@ class WishCraftedApp extends StatelessWidget {
             ),
             title: Lang[Words.appName],
             locale: value.currentLocale,
-            home: AccessibleApp(),
+            home: SplashScreen(),
             debugShowCheckedModeBanner: false,
           ),
         );
